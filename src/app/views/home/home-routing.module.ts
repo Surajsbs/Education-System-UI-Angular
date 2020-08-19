47771ee1227e70cd6ea8f884935b7af6d7bddf56
surajsbs <surajsavaratkar8080@gmail.com>
2020-08-19 19:05:00 +0530

    removed typo

--- src/app/views/home/home-routing.module.ts
+++ src/app/views/home/home-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
